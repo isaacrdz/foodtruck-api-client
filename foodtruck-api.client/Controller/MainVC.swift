99b4d39bf946736ad40f95127d51121a9e0876b2
isaacrdz <zack_cpp@hotmail.com>
2017-11-13 00:14:33 -0600

    first commit

---
+++ foodtruck-api.client/Controller/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainVC.swift
+//  foodtruck-api.client
+//
+//  Created by Isaac Rodriguez on 11/12/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+
+   
+
+}
