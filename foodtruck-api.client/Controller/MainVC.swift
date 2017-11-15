39e64fd88a3d8cb7ec5b4abb99c05cc760240353
isaacrdz <zack_cpp@hotmail.com>
2017-11-15 00:19:40 -0600

    MainVC

--- foodtruck-api.client/Controller/MainVC.swift
+++ foodtruck-api.client/Controller/MainVC.swift
@@ -9,26 +9,58 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataService = DataService.instance
+    var authService = Authservice.instance
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DataService.instance.delegate = self
+        
+        
+        
+        dataService.delegate = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.tableView.rowHeight = 120
         
         DataService.instance.getAllFoodTrucks()
     }
+    
 }
-    
-    
+
 extension MainVC: DataServiceDelegate {
-  
-    func trucksLoaded(){
+    func trucksLoaded() {
+        OperationQueue.main.addOperation {
+            self.tableView.reloadData()
+        }
     }
     
-    func reviewsLoaded(){
-        
+    func reviewsLoaded() {
+        // Do nothing
     }
 }
-   
 
-
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataService.foodTrucks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "FoodTruckCell", for: indexPath) as? FoodTruckCell {
+            cell.configureCell(truck: dataService.foodTrucks[indexPath.row])
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+}
---
+++ foodtruck-api.client/View/FoodTruckCell.swift
@@ -0,0 +1,34 @@
+//
+//  FoodTruckCell.swift
+//  foodtruck-api.client
+//
+//  Created by Isaac Rodriguez on 11/14/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FoodTruckCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var foodTypeLabel: UILabel!
+    @IBOutlet weak var avgCostLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(truck: FoodTruck) {
+        nameLabel.text = truck.name
+        foodTypeLabel.text = truck.foodType
+        avgCostLabel.text = "$\(truck.avgCost)"
+    }
+
+}
